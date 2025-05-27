2a8407721997ac692635fff8241d55e14b0c863c
rutikrtr <rutikshingote4325@gmail.com>
2025-05-27 18:41:36 +0530

    commit

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -27,3 +27,5 @@ export const logoutRestaurant = async () => {
   );
   return res.data;
 };
+
+
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,45 +1,51 @@
 // src/features/auth/authSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 import Cookies from 'js-cookie';
+
 const initialState = {
     user: Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null,
     restaurant: Cookies.get('restaurant') ? JSON.parse(Cookies.get('restaurant')) : null,
     accessToken: Cookies.get('accessToken') || null,
     refreshToken: Cookies.get('refreshToken') || null,
-    isAuthenticated: !!Cookies.get('accessToken')
+    isAuthenticated: !!Cookies.get('accessToken'),
 };
-
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
         loginSuccess: (state, action) => {
-            const { user, restaurant, accessToken, refreshToken } = action.payload;
+            const { user, restaurant, accessToken, refreshToken, pendingRestaurants, customers } = action.payload;
+
             state.user = user;
             state.restaurant = restaurant;
             state.accessToken = accessToken;
             state.refreshToken = refreshToken;
             state.isAuthenticated = true;
+            state.pendingRestaurants = pendingRestaurants || [];
+            state.customers = customers || [];
 
             Cookies.set('accessToken', accessToken, { secure: true });
             Cookies.set('refreshToken', refreshToken, { secure: true });
-
-           
         },
-
         logout: (state) => {
             state.user = null;
             state.restaurant = null;
             state.accessToken = null;
             state.refreshToken = null;
             state.isAuthenticated = false;
+            state.pendingRestaurants = [];
+            state.customers = [];
 
             Cookies.remove('accessToken');
             Cookies.remove('refreshToken');
-        }
+        },
     }
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const {
+    loginSuccess,
+    logout
+} = authSlice.actions;
+
 export default authSlice.reducer;
